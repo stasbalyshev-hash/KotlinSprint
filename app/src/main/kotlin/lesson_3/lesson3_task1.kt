766ed59945b44fa32e37a32ce1b3a2ccd5348868
Stas <stas.balyshev@mail.ru>
2025-08-30 13:48:38 +0300

    KS-3-1 arithmetic part1

---
+++ app/src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,10 @@
+package org.example.app.lesson_3
+
+fun main() {
+    val student1 = 3f
+    val student2 = 4f
+    val student3 = 3f
+    val student4 = 5f
+    val averageScore = ((student1 + student2 + student3 + student4) / 4)
+    println(averageScore)
+}
