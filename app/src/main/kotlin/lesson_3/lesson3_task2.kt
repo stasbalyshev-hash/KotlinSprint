23b82c6ceb632244a535167d29214d765cb3657f
Stas <stas.balyshev@mail.ru>
2025-08-31 22:11:33 +0300

    KS-2-2 arithmetic new solution

--- app/src/main/kotlin/lesson_3/lesson3_task2.kt
+++ app/src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -5,7 +5,8 @@ fun main() {
     val salaryOldWorker = 30000
     val interns = 30
     val salaryOfInterns = 20000
-    println("The total salary of all old workers: " + oldWorkers * salaryOldWorker)
-    println("Total salary costs for all workers: " + (oldWorkers * salaryOldWorker + interns * salaryOfInterns))
-    println("The average salary of one employee after the arrival of interns: " + (oldWorkers * salaryOldWorker + interns * salaryOfInterns) / (oldWorkers + interns))
+    val totalSalaryOldWorkers = oldWorkers * salaryOldWorker
+    println("The total salary of all old workers: " + totalSalaryOldWorkers)
+    println("Total salary costs for all workers: " + (totalSalaryOldWorkers + interns * salaryOfInterns))
+    println("The average salary of one employee after the arrival of interns: " + (totalSalaryOldWorkers + interns * salaryOfInterns) / (oldWorkers + interns))
 }
