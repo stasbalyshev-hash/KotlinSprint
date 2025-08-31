058f90df8b2983b38856b203ca720919ae06fba8
Stas <stas.balyshev@mail.ru>
2025-08-31 23:26:35 +0300

    KS-1-3 variables

--- app/src/main/kotlin/lesson_1/lesson1_task3.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,6 +1,14 @@
-package org.example.app.lesson_1
+package org.example.app.lesson_2
 
-fun main () {
-    println("Тестовое решение к уроку 1 задаче 3")
-    println("какие-то доработки")
+fun main() {
+    val year = "1961"
+    var hour = "09"
+    var minute = "07"
+    println("Takeoff time year: $year")
+    println("Takeoff time hour: $hour")
+    println("Takeoff time minute: $minute")
+    hour = "10"
+    minute = "55"
+    print("Boarding time: " + "$hour:" + "$minute")
+
 }
