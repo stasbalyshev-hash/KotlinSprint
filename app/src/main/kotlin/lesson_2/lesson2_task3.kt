cd3a66773d884ce122f69018b7cce41174e1d6c6
Stas <stas.balyshev@mail.ru>
2025-08-31 18:12:42 +0300

    KS-3-1 variables

--- app/src/main/kotlin/lesson_2/lesson2_task3.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -2,8 +2,8 @@ package org.example.app.lesson_2
 
 fun main() {
     val year = "1961"
-    var hour = "9"
-    var minute = "7"
+    var hour = "09"
+    var minute = "07"
     println("Takeoff time year: $year")
     println("Takeoff time hour: $hour")
     println("Takeoff time minute: $minute")
