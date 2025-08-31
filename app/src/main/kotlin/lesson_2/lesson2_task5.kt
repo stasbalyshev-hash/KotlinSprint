1104d9ab489ea7ddf369891342ce0156de8fde03
Stas <stas.balyshev@mail.ru>
2025-08-31 17:54:20 +0300

    KS-5-1 variables

--- app/src/main/kotlin/lesson_2/lesson2_task5.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,15 +1,17 @@
 package org.example.app.lesson_2
 
 import java.time.LocalDate
-
+const val part = 60 // кол-во секунд в минутах и минут в часах
 fun main() {
     val seconds = 6480
-    var minute = seconds / 60
+    val minute = seconds / part
     var hour = 0
-    if (minute/60 > 0)
         hour++
-        val minuteSpent: Int = minute % 60
-        val secondsSpent: Int = minuteSpent / 60
-    print("Time spent in space: " + "0$hour:" + "$minuteSpent:" + "0$secondsSpent")
+        val minutesSpent = minute % part
+        val secondsSpent = minutesSpent / part
+        val hourSpent = String.format("0%d",hour)
+        val secondsSpent1 = String.format("0%d",secondsSpent)
+    println(hourSpent + ":" + minutesSpent + ":" + secondsSpent1)
 }
+
 
