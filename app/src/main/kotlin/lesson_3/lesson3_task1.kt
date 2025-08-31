1a611fa3d73852c693041fd2f9f4a1ea26da1451
Stas <stas.balyshev@mail.ru>
2025-08-31 17:59:36 +0300

    KS-1-2 arithmetic

--- app/src/main/kotlin/lesson_3/lesson3_task1.kt
+++ app/src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -5,6 +5,6 @@ fun main() {
     val student2 = 4f
     val student3 = 3f
     val student4 = 5f
-    val averageScore = ((student1 + student2 + student3 + student4) / 4)
+    val averageScore = (student1 + student2 + student3 + student4) / 4
     println(averageScore)
 }
