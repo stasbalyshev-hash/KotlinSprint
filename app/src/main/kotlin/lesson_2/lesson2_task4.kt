99b5dd5fb767304360acaa27b81cedfd47b5f89e
Stas <stas.balyshev@mail.ru>
2025-09-01 22:06:50 +0300

    KS-2-4 arithmetic

--- app/src/main/kotlin/lesson_2/lesson2_task4.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task4.kt
@@ -1,11 +1,12 @@
 package org.example.app.lesson_2
-
+const val percent = 100
 fun main() {
     val crystal = 7
     val iron = 11
-    val buff = 0.2
-    val buffedCrystal = (crystal * buff).toInt()
-    val buffedIron = (iron * buff).toInt()
+    val buff = 20
+    val buffedCrystal = (crystal * buff) / percent
+    val buffedIron = (iron * buff) / percent
+
     println("buffedCrystal: $buffedCrystal")
     println("buffedIron: $buffedIron")
 }
