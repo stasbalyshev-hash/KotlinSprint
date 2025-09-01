fb44723dd454c9095cabaa065b2e35590c090557
Stas <stas.balyshev@mail.ru>
2025-09-01 23:03:41 +0300

    KS-1-2 variables

---
+++ app/src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,12 @@
+package org.example.app.lesson_1
+
+fun main() {
+    val countOfOrders = 75
+    val gratitudePeople = "Thanks for our order!"
+    println("Count of orders: $countOfOrders")
+    println(gratitudePeople)
+    val countOfWorker = 2000
+//    println("Count of worker in online store: $countOfWorker")
+    val countOfWorkerNew = 1999
+    println("Actual count of worker: $countOfWorkerNew")
+}
