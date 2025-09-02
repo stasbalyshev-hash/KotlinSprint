bee6bbf5df25fc41c8cc4711885179153532c0d5
Stas <stas.balyshev@mail.ru>
2025-09-02 20:22:14 +0300

    KS-2-5 arithmetic

--- app/src/main/kotlin/lesson_2/lesson2_task5.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,13 +1,14 @@
 package org.example.app.lesson_2
+
 import kotlin.math.pow
 
-const val percent = 100
+const val PERCENT = 100
 
 fun main() {
     val initialSum = 70000
     val rateOfInterest = 16.7
     val years = 20
-    val amountOfDeposit = initialSum * ((1 + (rateOfInterest / percent)).pow(years))
+    val amountOfDeposit = initialSum * ((1 + (rateOfInterest / PERCENT)).pow(years))
 
     println("The contribution amount in 20 years will be: ${"%.3f".format(amountOfDeposit)}")
 }
