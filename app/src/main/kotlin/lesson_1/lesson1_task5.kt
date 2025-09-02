1d63175547f9d7a7e58e7769d8fde2f11eaa59a0
Stas <stas.balyshev@mail.ru>
2025-09-02 20:07:10 +0300

    KS-1-5 variables

--- app/src/main/kotlin/lesson_1/lesson1_task5.kt
+++ app/src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -1,17 +1,14 @@
 package org.example.app.lesson_1
 
-const val part = 60
+const val PART = 60
 
 fun main() {
     val seconds = 6480
-    val minute = seconds / part
-    var hour = 0
-    hour++
-    val minutesSpent = minute % part
-    val secondsSpent = minutesSpent / part
-    val hourSpent = String.format("0%d",hour)
-    val secondsSpent1 = String.format("0%d",secondsSpent)
+    val minute = seconds / PART
+    val hour = minute / PART
+    val minutesSpent = minute % PART
+    val secondsSpent = minutesSpent / PART
 
-    println("Time spent in space: $hourSpent:$minutesSpent:$secondsSpent1")
+    println("%02d:%02d:%02d".format(hour, minutesSpent, secondsSpent))
 }
 
