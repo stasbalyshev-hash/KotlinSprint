8630022d13e832623d0ccfe8b3c1c88704c6487a
Stas <stas.balyshev@mail.ru>
2025-09-02 21:25:19 +0300

    KS-2-2 arithmetic

--- app/src/main/kotlin/lesson_2/lesson2_task2.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task2.kt
@@ -6,10 +6,11 @@ fun main() {
     val interns = 30
     val salaryOfInterns = 20000
     val totalSalaryOldWorkers = oldWorkers * salaryOldWorker  // подсчет расходов на выплаты постоянным сотрудникам
-    println("The total salary of all old workers: " + totalSalaryOldWorkers) // печать расходов на выплаты постоянным сотрудникам
     val totalSalaryInterns = interns * salaryOfInterns // подсчет общей выплаты интернам
     val totalSalary = totalSalaryOldWorkers + totalSalaryInterns // подсчет общей суммы зарплат всех работников
+    val averageSalary = totalSalary / (oldWorkers + interns) // подсчет средней зп после прихода стажеров
+
     println("Total salary costs for all workers: $totalSalary") // печать общей суммы зарплат всех работников
-    val averageSalary = (totalSalaryOldWorkers + totalSalaryInterns) / (oldWorkers + interns) // подсчет средней зп после прихода стажеров
+    println("The total salary of all old workers: $totalSalaryOldWorkers") // печать расходов на выплаты постоянным сотрудникам
     println("The average salary of one employee after the arrival of interns: $averageSalary") // печать средней зп после прихода стажеров
 }
