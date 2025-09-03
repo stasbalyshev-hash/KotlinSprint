0caddb53a7a8cc17f760c5813a911da62cc70adf
Stas <stas.balyshev@mail.ru>
2025-09-03 19:28:13 +0300

    KS-2-3 arithmetic_solved

--- app/src/main/kotlin/lesson_2/lesson2_task3.kt
+++ app/src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -1,14 +1,14 @@
 package org.example.app.lesson_2
 
-fun main() {
-    val year = 1961
-    var hour = 9
-    var minute = 7
-    println("Takeoff time year: $year")
-    println("Takeoff time hour: $hour")
-    println("Takeoff time minute: $minute")
-    hour = 10
-    minute = 55
-    print("Boarding time: " + "$hour:" + "$minute")
+const val PART = 60
 
+fun main() {
+    val hoursDeparture = 9
+    val minutesDeparture = 39
+    val travelTime = 457
+    val generalMinutesDeparture = hoursDeparture * PART + minutesDeparture + travelTime
+    val arrivalHours = generalMinutesDeparture / PART
+    val arrivalMinutes = generalMinutesDeparture % PART
+
+    println("%02d:%02d".format(arrivalHours,arrivalMinutes))
 }
