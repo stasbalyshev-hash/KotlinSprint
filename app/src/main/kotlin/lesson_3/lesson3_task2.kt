0e0e94b61a8cb11bd4ff9fd6caa1503bf9c65bbd
Stas <stas.balyshev@mail.ru>
2025-09-03 19:46:49 +0300

    KS-3-2 String

---
+++ app/src/main/kotlin/lesson_3/lesson3_task2.kt
@@ -0,0 +1,13 @@
+package org.example.app.lesson_3
+
+fun main() {
+    val name = "Tatiana"
+    val firstSurname = "Andreeva"
+    val secondSurname = "Sidorova"
+    val patronymic = "Sergeevna"
+    val year = 20
+    val year1 = 22
+
+    println("$firstSurname $name $patronymic, $year years")
+    println("$secondSurname $name $patronymic, $year1 years")
+}
