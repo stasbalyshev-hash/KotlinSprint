171b8504fa241c64621947a325960c480a834058
Stas <stas.balyshev@mail.ru>
2025-09-04 19:56:09 +0300

    KS-4-2 Comparison

--- app/src/main/kotlin/lesson_4/lesson4_task2.kt
+++ app/src/main/kotlin/lesson_4/lesson4_task2.kt
@@ -1,16 +1,19 @@
 package org.example.app.lesson_4
 
+const val MIN_AVERAGE = 35
+const val MAX_AVERAGE = 100
+const val VALUE = 100
+
 fun main() {
-    val minAverage = 35
-    val maxAverage = 100
-    val value = 100
     val weight1 = 20
     val value1 = 80
     val weight2 = 50
     val value2 = 100
-    val result1 = (weight1 > minAverage) && (weight1 <= maxAverage) && (value1 < value)
-    val result2 = (weight2 > minAverage) && (weight2 <= maxAverage) && (value2 < value)
+    val result1 = (weight1 > MIN_AVERAGE) && (weight1 <= MAX_AVERAGE) && (value1 < VALUE)
+    val result2 = (weight2 > MIN_AVERAGE) && (weight2 <= MAX_AVERAGE) && (value2 < VALUE)
 
-    println("Груз с весом $weight1 кг и объемом $value1 литров соответствует категории Average: $result1")
-    println("Груз с весом $weight1 кг и объемом $value1 литров соответствует категории Average: $result2")
+    println("Груз с весом $weight1 кг и объемом $value1 литров\n"
+            + "соответствует категории Average: $result1")
+    println("Груз с весом $weight2 кг и объемом $value2 литров\n"
+            + "соответствует категории Average: $result2")
 }
