083bbc53f57ec14ce8dd039a615e65aacc0e630a
Stas <stas.balyshev@mail.ru>
2025-09-05 20:22:48 +0300

    KS-4-4 Comparison

--- app/src/main/kotlin/lesson_4/lesson4_task4.kt
+++ app/src/main/kotlin/lesson_4/lesson4_task4.kt
@@ -1,16 +1,12 @@
 package org.example.app.lesson_4
 
-const val TRAINING_DAY = 5
-const val STEP = 4 // шаг между 2 одинаковыми тренировками
+const val TRAINING_DAY = 8
 
 fun main() {
+    val isEven = TRAINING_DAY % 2 == 0
 
-    var handsPlusAbs = 1
-    var backPlusLegs = 3
-
-
-    println("Упражнения для рук: ${TRAINING_DAY % STEP == handsPlusAbs}\n"
-            + "Упражнения для ног: ${TRAINING_DAY % STEP == backPlusLegs}\n"
-            + "Упражнения для спины: ${TRAINING_DAY % STEP == backPlusLegs}\n"
-            + "Упражнения для пресса: ${TRAINING_DAY % STEP == handsPlusAbs}\n")
+    println("Упражнения для рук: ${!isEven}\n"
+            + "Упражнения для ног: $isEven\n"
+            + "Упражнения для спины: $isEven\n"
+            + "Упражнения для пресса: ${!isEven}\n")
 }
