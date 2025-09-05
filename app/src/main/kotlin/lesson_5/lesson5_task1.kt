1ea0cdd1a83fcbdbb6ea19750c681c5365b4df4a
Stas <stas.balyshev@mail.ru>
2025-09-05 23:44:39 +0300

    KS-5-1 conditions

--- app/src/main/kotlin/lesson_5/lesson5_task1.kt
+++ app/src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,10 +1,17 @@
 package org.example.app.lesson_5
 
-fun main() {
-    println("Решите пример: 2 + 2 = ")
+import kotlin.random.Random
 
-    val input = readln().toInt()
-    if (input == 4) {
+fun main() {
+    val random = Random
+    val firstNumber = random.nextInt(99)
+    val secondNumber = random.nextInt(99)
+    val sum = firstNumber + secondNumber
+
+    println("Решите пример: $firstNumber + $secondNumber = ")
+
+    val userAnswer = readln().toInt()
+    if (userAnswer == sum) {
         println("Добро пожаловать!")
     } else {
         println("Доступ запрещен.")
