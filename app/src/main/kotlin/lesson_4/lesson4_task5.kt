792f7c9856c9aa1a9de81f62ba812537ffbefde4
Stas <stas.balyshev@mail.ru>
2025-09-05 23:16:29 +0300

    KS-5-4 comparison

--- app/src/main/kotlin/lesson_4/lesson4_task5.kt
+++ app/src/main/kotlin/lesson_4/lesson4_task5.kt
@@ -1,26 +1,24 @@
 package org.example.app.lesson_4
 
-const val IS_SHIP_DAMAGE1 = false
-const val COUNT_BOX_PROVISION1 = 50
-
-const val IS_SHIP_DAMAGE2 = true
-const val COUNT_OF_CREW2 = 70
-const val COUNT_BOX_PROVISION2 = 50
-const val IS_FAVORABLE_WEATHER2: Boolean = true
+const val IS_SHIP_DAMAGE = false
+const val COUNT_OF_CREW = 55
+const val COUNT_OF_CREW_VARIABLE = 70
+const val COUNT_BOX_PROVISION = 50
+const val IS_FAVORABLE_WEATHER: Boolean = true
 
 fun main() {
     val isShipDamageInput = readln().toBoolean()
     val countOfCrewInput = readln().toInt()
     val countBoxProvisionsInput = readln().toInt()
     val isFavorableWeatherInput = readln().toBoolean()
-    val countOfCrew1 = 55..70
-    val result1: Boolean = isShipDamageInput == IS_SHIP_DAMAGE1 &&
+    val countOfCrew1 = COUNT_OF_CREW..COUNT_OF_CREW_VARIABLE
+    val result1: Boolean = isShipDamageInput == IS_SHIP_DAMAGE &&
             countOfCrewInput in countOfCrew1 &&
-            countBoxProvisionsInput > COUNT_BOX_PROVISION1
-    val result2: Boolean = isShipDamageInput == IS_SHIP_DAMAGE2 &&
-            countOfCrewInput == COUNT_OF_CREW2 &&
-            countBoxProvisionsInput >= COUNT_BOX_PROVISION2 &&
-            isFavorableWeatherInput == IS_FAVORABLE_WEATHER2
+            countBoxProvisionsInput > COUNT_BOX_PROVISION
+    val result2: Boolean = isShipDamageInput == IS_SHIP_DAMAGE &&
+            countOfCrewInput == COUNT_OF_CREW_VARIABLE &&
+            countBoxProvisionsInput >= COUNT_BOX_PROVISION &&
+            isFavorableWeatherInput == IS_FAVORABLE_WEATHER
 
-            println("Корабль может отправиться в плавание: ${result1 || result2}")
+    println("Корабль может отправиться в плавание: ${result1 || result2}")
 }
