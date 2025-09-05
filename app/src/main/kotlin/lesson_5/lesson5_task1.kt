a78589b9d6ac134257fddd9a1ffa15e24076be9d
Stas <stas.balyshev@mail.ru>
2025-09-05 19:48:31 +0300

    KS-5-1 conditions

---
+++ app/src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -0,0 +1,12 @@
+package org.example.app.lesson_5
+
+fun main() {
+    println("Решите пример: 2 + 2 = ")
+
+    val input = readln().toInt()
+    if (input == 4) {
+        println("Добро пожаловать!")
+    } else {
+        println("Доступ запрещен.")
+    }
+}
