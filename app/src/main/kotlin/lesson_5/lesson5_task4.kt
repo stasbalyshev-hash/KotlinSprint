2a1d9ee727348281a6dc179b8317384ebf1dcc9d
Stas <stas.balyshev@mail.ru>
2025-09-06 13:26:44 +0300

    KS-5-4 conditions

---
+++ app/src/main/kotlin/lesson_5/lesson5_task4.kt
@@ -0,0 +1,30 @@
+package org.example.app.lesson_5
+
+const val USER_NAME = "Zaphod"
+const val USER_PASSWORD = "PanGalactic"
+
+fun main() {
+
+    val inputUserName = readln().toString()
+    val inputUserPassword = readln().toString()
+
+    if (inputUserName == USER_NAME) {
+        if (inputUserPassword == USER_PASSWORD) {
+            println("[вздыхает...] Ваши данные проверены, и о, чудо, они верны... Пользователь \"Zaphod\", " +
+                        "вам разрешено входить на борт корабля \"Heart of Gold\". Хотя мне всё равно... " +
+                        "Ну вперед, войдите... Если вам так уж надо, в конце концов... [меланхолический вздох...] " +
+                        "Надеюсь, вам понравится пребывание здесь больше, чем мне.")
+        } else {
+            println("К сожалению, вы ввели неверные данные")
+        }
+    } else {
+        println("Пожалуйста зарегистрируйтесь и закончите процесс тут")
+    }
+}
+
+
+      
+
+           
+
+
