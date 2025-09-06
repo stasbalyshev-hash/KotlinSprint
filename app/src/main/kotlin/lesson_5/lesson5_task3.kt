35770e36e726b35ea8b4fe2e2040e3fd0d749de5
Stas <stas.balyshev@mail.ru>
2025-09-06 12:15:33 +0300

    KS-5-3 conditions

--- app/src/main/kotlin/lesson_5/lesson5_task3.kt
+++ app/src/main/kotlin/lesson_5/lesson5_task3.kt
@@ -1,23 +1,23 @@
 package org.example.app.lesson_5
 
 fun main() {
-    val FIRST_LUCKY_NUMBER = (0..42).random()
-    val SECOND_LUCKY_NUMBER = (0..42).random()
+    val firstLuckyNumber = (0..42).random()
+    val secondLuckyNumber = (0..42).random()
 
     println("Попытайтесь угадать 2 числа:")
 
     val inputNumber1 = readln().toInt()
     val inputNumber2 = readln().toInt()
-    if ((inputNumber1 == FIRST_LUCKY_NUMBER && inputNumber2 == SECOND_LUCKY_NUMBER) ||
-        (inputNumber1 == SECOND_LUCKY_NUMBER && inputNumber2 == FIRST_LUCKY_NUMBER))
+    if ((inputNumber1 == firstLuckyNumber && inputNumber2 == secondLuckyNumber) ||
+        (inputNumber1 == secondLuckyNumber && inputNumber2 == firstLuckyNumber))
      {
         println("Поздравляем! Вы выиграли главный приз!")
-    } else if ((inputNumber1 == FIRST_LUCKY_NUMBER || inputNumber1 == SECOND_LUCKY_NUMBER) ||
-        (inputNumber2 == FIRST_LUCKY_NUMBER || inputNumber2 == SECOND_LUCKY_NUMBER))
+    } else if ((inputNumber1 == firstLuckyNumber || inputNumber1 == secondLuckyNumber) ||
+        (inputNumber2 == firstLuckyNumber || inputNumber2 == secondLuckyNumber))
      {
         println("Вы выиграли утешительный приз!")
     } else {
         println("Неудача!")
     }
-    println("Для победы нужны были числа: $FIRST_LUCKY_NUMBER и $SECOND_LUCKY_NUMBER")
+    println("Для победы нужны были числа: $firstLuckyNumber и $secondLuckyNumber")
 }
